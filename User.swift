271b7f01d77263cf935648232159cd803373121f
GalileArtea <al02797904@tecmilenio.mx>
2019-05-10 14:32:35 -0500

    Login y Splash Screen
    Evidencia 1, diseño de la interfaz del login además de añadir el splash screen.

---
+++ User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Pinterest
+//
+//  Created by Luna Galilea Artea Magaña on 3/14/19.
+//  Copyright © 2019 Luna Galilea Artea Magaña. All rights reserved.
+//
+
+import Foundation
+class User {
+    var email: String?
+    var password: String?
+    var age: String?    
+}
